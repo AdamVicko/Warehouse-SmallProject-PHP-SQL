cd8160134503ac146b6b08d15cc69b9891fb6db9
AdamVicko <vickovic2203@gmail.com>
2022-12-16 22:47:52 +0100

    Creating home page
    Created login page (index.html) and home page after log in (home.html).
    Added JS script for the functionality of login page.
    Created css .

---
+++ www/vhost.hr/js/app.js
@@ -0,0 +1,13 @@
+
+//Pozivanje pop-up modala i sakrivanje svega "iza njega"
+document.querySelector(`#registration`).addEventListener(`click`, () => {
+    document.querySelector(`.custom-modal`).style.display = `block`
+    document.querySelector(`.main-wrapper`).style.display = `none`
+    document.querySelector(`.no-acc-register`).style.display = `none`
+});
+//Gasenje modala na x i pozivanje svega na stranici nazad
+document.querySelector(`#closeModal`).addEventListener(`click`, () => {
+    document.querySelector(`.custom-modal`).style.display = `none`
+    document.querySelector(`.main-wrapper`).style.display = `block`
+    document.querySelector(`.no-acc-register`).style.display = `block`
+});
