cae86a42e19e01f063a57839e7f364ba3dbf38a5
AdamVicko <vickovic2203@gmail.com>
2023-01-04 22:14:05 +0100

    Rearranging
    Rearranging files.

--- www/edunovaHOST.hr/bootstrap/home.php
+++ www/edunovaHOST.hr/bootstrap/home.php
@@ -5,6 +5,7 @@
 
     <?php include_once 'head.php'; ?>
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/add_patient_modal.css">
 </head>
 
 <body>
@@ -57,51 +58,7 @@
                         </ul>
                     </li>
 
-                    <!--POP UP MODAL-->
-                    <div class="custom2-modal">
-
-                        <button id="close_custom2_modal">X</button>
-
-                            <form id="add_patient">
-                                <div>
-                                    <label>Name and Surname:</label>
-                                    <input type="text" name="register_patient_name" id="register_patient_name" placeholder="Name and Surname">
-                                </div>
-
-                                <div>
-                                    <label>Adress:</label>
-                                    <input type="text" name="register_patient_adress" id="register_patient_adress" placeholder="Adress">
-                                </div>
-
-                                <div>
-                                    <label>Phone:</label>
-                                    <input type="text" name="register_patient_phone" id="register_patient_phone" placeholder="09...">
-                                </div>
-
-                                <div>
-                                    <label>Serial Number:</label>
-                                    <input type="text" name="register_patient_serial_number" id="register_patient_serial_number" placeholder="OXYGEN">
-                                </div>
-
-                                <div>
-                                    <label>Date of delivery:</label>
-                                    <input type="date" name="delivery_date" id="delivery_date" placeholder="01.01.2000">
-                                </div>
-
-                                <div>
-                                    <label>Date of collection:</label>
-                                    <input type="date" name="collection_date" id="collection_date" placeholder="01.01.2000">
-                                </div>
-
-                                <div>
-                                    <label>Comment:</label>
-                                    <textarea class="form-control"></textarea>
-                                </div>
-
-                            </form>
-
-                            <button id="confirm_patient">Confirm Patient</button>
-                    </div>
+                    <?php include_once 'add_patient_modal.php' ?>
 
                 </ul>
             </div>
@@ -236,7 +193,7 @@
     <?php include_once 'footer.php'; ?>
 
     <?php include_once 'scripts.php'; ?>
-    <script src="js/home.js"></script>
+    <script src="js/app.js"></script>
 </body>
 
 </html>
--- www/edunovaHOST.hr/bootstrap/patients.php
+++ www/edunovaHOST.hr/bootstrap/patients.php
@@ -26,8 +26,13 @@
                             <a class="nav-link" aria-current="page" href="home.php">Home</a>
                         </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Patients</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Patients
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><button class="dropdown-item" id="add_patient_popup_btn" >Add Patient</button></li>
+                            </ul>
                         </li>
 
                         <li class="nav-item">
@@ -45,6 +50,53 @@
                                 <li><a class="dropdown-item" href="#">All transactions</a></li>
                             </ul>
                         </li>
+
+                        <!--POP UP MODAL-->
+                        <div class="custom2-modal">
+
+                            <button id="close_custom2_modal">X</button>
+
+                            <form id="add_patient">
+                                <div>
+                                    <label>Name and Surname:</label>
+                                    <input type="text" name="register_patient_name" id="register_patient_name" placeholder="Name and Surname">
+                                </div>
+
+                                <div>
+                                    <label>Adress:</label>
+                                    <input type="text" name="register_patient_adress" id="register_patient_adress" placeholder="Adress">
+                                </div>
+
+                                <div>
+                                    <label>Phone:</label>
+                                    <input type="text" name="register_patient_phone" id="register_patient_phone" placeholder="09...">
+                                </div>
+
+                                <div>
+                                    <label>Serial Number:</label>
+                                    <input type="text" name="register_patient_serial_number" id="register_patient_serial_number" placeholder="OXYGEN">
+                                </div>
+
+                                <div>
+                                    <label>Date of delivery:</label>
+                                    <input type="date" name="delivery_date" id="delivery_date" placeholder="01.01.2000">
+                                </div>
+
+                                <div>
+                                    <label>Date of collection:</label>
+                                    <input type="date" name="collection_date" id="collection_date" placeholder="01.01.2000">
+                                </div>
+
+                                <div>
+                                    <label>Comment:</label>
+                                    <textarea class="form-control"></textarea>
+                                </div>
+
+                            </form>
+
+                            <button id="confirm_patient">Confirm Patient</button>
+                        </div>
+
                     </ul>
                 </div>
 
@@ -184,23 +236,12 @@
 
     </div>
     
-</div>
-
-
-
-
-</div>
-
-
-
-
-
-
-
 
     <!--Footer-->
     <?php include_once 'footer.php'; ?>
 
     <?php include_once 'scripts.php'; ?>
+    <script src="js/app.js"></script>
+
 </body>
 </html>
